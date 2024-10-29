199c449b55c5a8f05bb1a5a9f296e9bfd49017bd
Alex <alexander.wilden@gmail.com>
2024-10-29 21:26:33 +0100

    Added missing exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export { type Constructor } from './Constructor';
 export { jsonArrayInstanceDeserializer } from './JsonArrayInstanceDeserializer';
-export { jsonDateDeserializer } from './JsonDateDeserializer';
-export { jsonInstanceDeserializer } from './JsonInstanceDeserializer';
+export { jsonDateDeserializer, jsonNullableDateDeserializer } from './JsonDateDeserializer';
+export { jsonInstanceDeserializer, jsonNullableInstanceDeserializer } from './JsonInstanceDeserializer';
 export { asInstance, asInstances, jsonProperty, parseAsInstance, parseAsInstances } from './JsonProperty';
+export { type Nullable } from './Nullable';
+
